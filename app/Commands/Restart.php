075f8d87fd6c508e00d6c7b199899366a802229b
Imad Kada <i.kada@abbatis.nl>
2019-09-25 15:34:58 +0200

    fix restart single service command

--- app/Commands/Restart.php
+++ app/Commands/Restart.php
@@ -31,11 +31,13 @@ class Restart extends StackCommand
             if (empty($choice) || $server->label() === $choice) {
                 $this->output->note("Restarting {$server->label()}");
                 $server->restart();
+
+                return $this->success("Successfully restarted {$choice}!");
             }
         }
 
         if (empty($choice)) {
-            return $this->success("Done!");
+            return $this->success("Successfully restart all services!");
         }
 
         return $this->fail($choice . " does not seem like a valid server.");
