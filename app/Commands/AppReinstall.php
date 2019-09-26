3dda6a305f3e6e1670ff2b794518973d3fe01c9b
Imad Kada <i.kada@me.com>
2019-09-26 12:42:38 +0200

    add app:reinstall command

---
+++ app/Commands/AppReinstall.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Commands;
+
+use App\Shell;
+use LaravelZero\Framework\Commands\Command;
+
+class AppReinstall extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reinstall';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Reinstall the latest version of the application.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->output->writeln("Downloading install script...");
+        Shell::cmd('curl -s --progress-bar https://stew.sh/install | bash', $this->output);
+
+        return 0;
+    }
+}
