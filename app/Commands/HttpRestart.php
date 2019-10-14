7cf6bf29586da156392e139189cf775ee5a86d2d
Imad Kada <i.kada@abbatis.nl>
2019-10-14 14:36:31 +0200

    add seperate cli output for http conf rewrite

--- app/Commands/HttpRestart.php
+++ app/Commands/HttpRestart.php
@@ -25,9 +25,10 @@ class HttpRestart extends StackCommand
      */
     public function process(): int
     {
-        $this->output->note("Restarting caddy http server...");
-
+        $this->output->note("Writing caddy http configuration files...");
         $this->stack->httpServer()->config()->save();
+
+        $this->output->note("Restarting caddy http server...");
         $this->stack->httpServer()->restart();
 
         $this->output->success("Done!");
